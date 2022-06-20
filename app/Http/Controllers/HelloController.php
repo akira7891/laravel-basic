684ee616067a652755cc720c06984e0d4047cd51
akira <akiraakira462@gmail.com>
2022-06-21 06:36:37 +0900

    ゲットのクエリ文字列の出力

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,14 +10,7 @@ class HelloController extends Controller
   
 public function index(Request $request,Response $response)
 {
-    $html=<<<eof
-    <pre>$request</pre>
-    <br><br><br>
-    <pre>$response</pre>
-    <p></p>
-    eof;
-
-    return $html;
-
+    $queri=$request->akira;
+    return "$queri";
 }
 }
